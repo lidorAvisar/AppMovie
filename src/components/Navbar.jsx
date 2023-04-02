4a0d938ea709df0a3887ef86ec92e7a5f744e08a
lidor avisar <avisar0402@gmail.com>
2023-04-02 17:27:28 +0300

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        <label className='text-black' htmlFor="yessssss"></label>
-        <input type="text" placeholder='dfdsff' />
+      
     </div>
   )
 }
