b5de7ac5feab48471528f7e752c2c3c2045ea473
lidor avisar <avisar0402@gmail.com>
2023-04-02 17:19:33 +0300

    ok

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <label className='text-black' htmlFor="yessssss"></label>
+        <input type="text" placeholder='dfdsff' />
+    </div>
+  )
+}
---
+++ src/routes/Approuters.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Approuters() {
+  return (
+    <div>Approuters</div>
+  )
+}
